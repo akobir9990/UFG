fcab8d326e49016e94792e986368349af615c0db
akobir9990 <akobir.qoshshayev@gmail.com>
2023-09-28 06:37:50 +0500

    added video

---
+++ src/pages/videos/videosData.js
@@ -0,0 +1,21 @@
+import one from "./img/1.svg";
+// import two from "./img/2.svg";
+// import three from "./img/3.svg";
+// import four from "./img/4.svg";
+// import five from "./img/5.svg";
+// import six from "./img/6.svg";
+// import seven from "./img/7.svg";
+// import eight from "./img/8.svg";
+// import nine from "./img/9.svg";
+// import ten from "./img/10.svg";
+
+export const items = [
+  {
+    id: 1,
+    name: "1-тема",
+    url: "",
+    content: "content - 1",
+    panel: "panel1",
+    img: one,
+  },
+];
